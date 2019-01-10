51b676f5b62dc644a4113ab6d30419f721ceab61
Abhishek Katiyar <abhishekkatiyar049@gmail.com>
2019-01-10 13:23:18 +0530

    work in progress

--- lib/rna_transcription.rb
+++ lib/rna_transcription.rb
@@ -1,25 +1,24 @@
 class RnaTranscription
   def self.complement_of_dna(str)
-    l=str.length
-    str2=""
-    i=0
-    if l==0
+    l = str.length
+    str2 = ""
+    i = 0
+    if l == 0
       return ""
-    elsif
-      while i < l
-        if str[i]=='A'
-          str2=str2+'U'
-        elsif str[i]=='C'
-          str2=str2+'G'
-        elsif str[i]=='G'
-          str2=str2+'C'
-        elsif str[i]='T'
-          str2=str2+'A'
-        end
-        i=i+1;
-      end
-      end
-    return str2
+    elsif while i < l
+            if str[i] == 'A'
+              str2 = str2 + 'U'
+            elsif str[i] == 'C'
+              str2 = str2 + 'G'
+            elsif str[i] == 'G'
+              str2 = str2 + 'C'
+            elsif str[i] = 'T'
+              str2 = str2 + 'A'
+            end
+            i = i + 1;
+          end
     end
+    return str2
   end
+end
 
